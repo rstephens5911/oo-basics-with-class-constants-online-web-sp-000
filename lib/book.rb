c6f227c63b25d82bba7e8c4867a181a1d12507f3
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-31 23:36:45 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
   GENRES = []
 
@@ -8,16 +8,13 @@ class Book
     @title = title
   end
 
-  def genre= (genre)
-    @genre = genre
-  end
-
-  def genre
-    @genre
-  end
-
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
+    def genre= (genre)
+      @genre = genre
+      GENRES << genre
+    end
 
 end
